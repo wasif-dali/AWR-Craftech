ab8747331593f367349f25e4645cc9ed4961d16a
Wasif Ali <wasifdaliali@gmail.com>
2025-05-19 23:51:45 +0530

    start with server and add mongo db data base and start with client and start react install vite

---
+++ server/server.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const mongoose = require('mongooose')
+mongoose.connect('mongodb+srv://awrcraftechsre:R57TPRDCRL6guqlW@awrcraftech.bnfdnk6.mongodb.net/')
+.then(()
+const app =express()
+const PORT = process.env.PORT || 5000;
