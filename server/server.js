5758aa84b4c298c55e932c05734fbbbe8dc9a5d1
Wasif Ali <wasifdaliali@gmail.com>
2025-05-21 23:39:29 +0530

    connect with mongodb and install some library

--- server/server.js
+++ server/server.js
@@ -1,6 +1,31 @@
 const express = require('express')
-const mongoose = require('mongooose')
-mongoose.connect('mongodb+srv://awrcraftechsre:R57TPRDCRL6guqlW@awrcraftech.bnfdnk6.mongodb.net/')
-.then(()
+const mongoose = require('mongoose')
+const cookieParser =require('cookie-parser');
+const cors= require('cors');
+mongoose
+.connect(
+    "mongodb+srv://awrcraftechsre:QtjdkqTj0kM1QhcJ@awrcraftech.bpvmssk.mongodb.net/"
+)
+.then(()=>console.log('MongoDB connected')).catch((error) => console.log(error));
 const app =express()
 const PORT = process.env.PORT || 5000;
+
+app.use(
+     cors({
+        origin : 'http://localhost:5173/',
+        method : ['GET','POST','DELETE','PUT'],
+        allowedHeaders : [
+            "Content-Type",
+            'Authorization',
+            'Cache-Control',
+            'Exprires',
+            'Pragma'
+        ],
+        Credentials : true 
+
+     })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT,()=> console.log(`Server is now running on the port ${PORT}`))
